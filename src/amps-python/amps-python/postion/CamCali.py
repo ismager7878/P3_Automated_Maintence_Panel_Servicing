5cf961391e2b28f46afe26d51b05a15677c440cd
Petur <phamme24@student.aau.dk>
2025-10-16 11:31:07 +0200

    påbegynds ROS2 implementering

--- src/amps-python/amps-python/postion/CamCali.py
+++ src/amps-python/amps-python/postion/CamCali.py
@@ -2,55 +2,113 @@ import cv2 as cv
 import cv2.aruco as aruco
 import numpy as np
 import glob
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
 
-# cam_cali kan ikke laves før vi har data fra et checker board
-def cam_cali():
-    # Antal indre hjørner i dit checkerboard
-    CHESSBOARD_SIZE = (9, 6)
+class CameraCalibration(Node):
 
-    # Reelle fysiske mål for ét kvadrat (i meter eller mm)
-    SQUARE_SIZE = 0.025  # 25 mm
+    def __init__(self):
+        super().__init__('camera_calibration')
+        self.subscription = self.create_subscription(
+            int,               # message type
+            'videoFeed',            # topic name
+            self.listener_callback,  # callback function
+            10                    # QoS (queue size)
+        )
+        self.subscription  # prevent unused variable warning
 
-    # Klargør objektpunkter (3D punkter i "virkeligheden")
-    objp = np.zeros((CHESSBOARD_SIZE[0]*CHESSBOARD_SIZE[1], 3), np.float32)
-    objp[:, :2] = np.mgrid[0:CHESSBOARD_SIZE[0], 0:CHESSBOARD_SIZE[1]].T.reshape(-1, 2)
-    objp *= SQUARE_SIZE
+    def data_collector():
+        pass
 
-    # Lister til punkter fra alle billeder
-    objpoints = []  # 3D points
-    imgpoints = []  # 2D points
+    # cam_cali kan ikke laves før vi har data fra et checker board
+    def cam_cali():
+        # Antal indre hjørner i dit checkerboard
+        CHESSBOARD_SIZE = (9, 6)
 
-    images = glob.glob('calib_images/*.jpg')
+        # Reelle fysiske mål for ét kvadrat (i meter eller mm)
+        SQUARE_SIZE = 0.025  # 25 mm
 
-    for fname in images:
-        img = cv.imread(fname)
-        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        # Klargør objektpunkter (3D punkter i "virkeligheden")
+        objp = np.zeros((CHESSBOARD_SIZE[0]*CHESSBOARD_SIZE[1], 3), np.float32)
+        objp[:, :2] = np.mgrid[0:CHESSBOARD_SIZE[0], 0:CHESSBOARD_SIZE[1]].T.reshape(-1, 2)
+        objp *= SQUARE_SIZE
 
-        # Find checkerboard corners
-        ret, corners = cv.findChessboardCorners(gray, CHESSBOARD_SIZE, None)
+        # Lister til punkter fra alle billeder
+        objpoints = []  # 3D points
+        imgpoints = []  # 2D points
 
-        if ret:
-            objpoints.append(objp)
-            imgpoints.append(corners)
+        images = glob.glob('calib_images/*.jpg')
 
-            # (valgfrit) vis hjørner
-            cv.drawChessboardCorners(img, CHESSBOARD_SIZE, corners, ret)
-            cv.imshow('Corners', img)
-            cv.waitKey(100)
+        for fname in images:
+            img = cv.imread(fname)
+            gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    cv.destroyAllWindows()
+            # Find checkerboard corners
+            ret, corners = cv.findChessboardCorners(gray, CHESSBOARD_SIZE, None)
 
-    ret, cameraMatrix, distCoeffs, rvecs, tvecs = cv.calibrateCamera(
-    objpoints, imgpoints, gray.shape[::-1], None, None)
+            if ret:
+                objpoints.append(objp)
+                imgpoints.append(corners)
 
-    print("Camera matrix:\n", cameraMatrix)
-    print("Distortion coefficients:\n", distCoeffs)
+                # (valgfrit) vis hjørner
+                cv.drawChessboardCorners(img, CHESSBOARD_SIZE, corners, ret)
+                cv.imshow('Corners', img)
+                cv.waitKey(100)
 
-    img = cv.imread('calib_images/img1.jpg')
-    h, w = img.shape[:2]
-    newCameraMatrix, roi = cv.getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, (w,h), 1, (w,h))
-    undistorted = cv.undistort(img, cameraMatrix, distCoeffs, None, newCameraMatrix)
-    cv.imshow('Original', img)
-    cv.imshow('Undistorted', undistorted)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+        cv.destroyAllWindows()
+
+        ret, cameraMatrix, distCoeffs, rvecs, tvecs = cv.calibrateCamera(
+        objpoints, imgpoints, gray.shape[::-1], None, None)
+
+        print("Camera matrix:\n", cameraMatrix)
+        print("Distortion coefficients:\n", distCoeffs)
+
+        img = cv.imread('calib_images/img1.jpg')
+        h, w = img.shape[:2]
+        newCameraMatrix, roi = cv.getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, (w,h), 1, (w,h))
+        undistorted = cv.undistort(img, cameraMatrix, distCoeffs, None, newCameraMatrix)
+        cv.imshow('Original', img)
+        cv.imshow('Undistorted', undistorted)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
+    # handeyeCali skal have data fra både robotten og cameraet.
+    def hand_eye_cali():
+        #R: rotation
+        #t. translation
+
+        R_gripper2base = [np.eye(3),
+                          np.eye(3),
+                          np.eye(3)]
+        
+        t_gripper2base = [np.array([[0.0, 0.0, 0.0]]).T,
+                          np.array([[0.1, 0.0, 0.0]]).T,
+                          np.array([[0.0, 0.1, 0.0]]).T]
+
+        R_target2cam = [np.eye(3),
+                        np.eye(3),
+                        np.eye(3)]
+
+        t_target2cam = [np.array([[0.0, 0.0, 0.5]]).T,
+                        np.array([[0.05, 0.0, 0.5]]).T,
+                        np.array([[0.0, 0.05, 0.5]]).T]
+        
+        #this function outputs in quaternion:
+        R_cam2gripper, t_cam2gripper = cv.calibrateHandEye(R_gripper2base, 
+                                                            t_gripper2base, 
+                                                            R_target2cam, 
+                                                            t_target2cam, 
+                                                            None, 
+                                                            None, 
+                                                            cv.CALIB_HAND_EYE_TSAI)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = CameraCalibration()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
