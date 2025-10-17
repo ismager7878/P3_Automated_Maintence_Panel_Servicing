719c5317c4170b51b0b0db017128825c7f480e9f
Petur <phamme24@student.aau.dk>
2025-10-17 16:46:26 +0200

    hjælperfunktion fra quaternion til axis angle lavet

--- src/amps-python/amps-python/postion/Cam_cali_data_collect.py
+++ src/amps-python/amps-python/postion/Cam_cali_data_collect.py
@@ -1,8 +1,30 @@
 import yaml
+import spatialmath as spm
+import numpy as np
 
-# Åbn og læs YAML-filen
-with open("/home/petur/Documents/Github/P3_Automated_Maintence_Panel_Servicing/src/amps-python/amps-python/data/calibration-data/camera_calibration.yaml", "r") as file:
-    data = yaml.safe_load(file)
+def quaternion2axis_angle(e1, e2, e3, e4):
+    q = spm.UnitQuaternion([e4, e1, e2, e3])
+    R = q.R
 
-# Print indholdet som et Python dictionary
-print(data)
+    np.set_printoptions(precision=4, suppress=True)  # Set print options for better readability
+
+    r_mat = spm.SO3(R)
+    theta, Kt = r_mat.angvec()
+
+    return Kt, theta
+
+Kt, theta = quaternion2axis_angle(0.064071, 0.091158, 0.15344, 0.98186)
+
+print("Axis angle from quaternion:")
+print(f"theta: {theta}")
+print(f"Kt: {Kt}")
+
+def yaml_files():
+
+    # Åbn og læs YAML-filen
+    with open("/home/petur/Documents/Github/P3_Automated_Maintence_Panel_Servicing/src/amps-python/amps-python/data/calibration-data/camera_calibration.yaml", "r") as file:
+        data = yaml.safe_load(file)
+
+    # Print indholdet som et Python dictionary
+    print(data)
+
