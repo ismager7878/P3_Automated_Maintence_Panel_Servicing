249df9cbdef7c695a27f9a30873b8571cd7d99cf
Benjamin Hermansen <bshermansen@gmail.com>
2025-10-31 13:49:51 +0100

    Start tracking dataset for board configurations

--- src/amps_python/setup.py
+++ src/amps_python/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'amps_python'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # install any python launch files so `ros2 launch <pkg> <file>` can find them
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
