10db46e5759d346083691d4adde1179d35e05d55
Benjamin Hermansen <bshermansen@gmail.com>
2025-11-04 21:14:19 +0100

    added a testing function for image processing

---
+++ src/amps_python/amps_python/resources/test.py
@@ -0,0 +1,48 @@
+from border_detector_color import detect_border
+import csv, json, os
+import numpy as np
+import cv2 as cv
+
+# Load image paths from file
+#! Is currently set to load training data
+color_paths = []
+depth_paths = []
+
+print("Reading CSV file")
+with open("datasets/test_images_dataset/training_paths.csv", "r") as file:
+    csvFile = csv.reader(file)
+    for line in csvFile:
+        print(f"Reading images in: {line[0]}", end="\r", flush=True)
+        color_paths.append(os.path.join(line[0], "color.png"))
+        depth_paths.append(os.path.join(line[0], "depth.png"))
+
+# Load images from paths
+color_images = np.array([])
+depth_images = np.array([])
+
+print("Loading color images")
+for i, path in enumerate(color_paths):
+    print(f"{i+1} of {len(color_paths)}", end="\r")
+    img = cv.imread(path)
+    color_images = np.append(color_images, img)
+print()
+
+print("Loading depth images")
+for i, path in enumerate(depth_paths):
+    print(f"{i+1} of {len(depth_paths)}", end="\r")
+    img = cv.imread(path)
+    depth_images = np.append(depth_images, img) 
+print()
+
+#Load ground truths
+ground_truth_btn_config_1 = json.load("datasets/test_images_dataset/btn_config_1/ground_truth.json")
+ground_truth_btn_config_2 = json.load("datasets/test_images_dataset/btn_config_2/ground_truth.json")
+ground_truth_btn_config_3 = json.load("datasets/test_images_dataset/btn_config_3/ground_truth.json")
+ground_truth_btn_config_4 = json.load("datasets/test_images_dataset/btn_config_4/ground_truth.json")
+ground_truth_btn_config_5 = json.load("datasets/test_images_dataset/btn_config_5/ground_truth.json")
+
+if __name__ == "__main__":
+    for image in color_images:
+        detect_border(image, True)
+
+
