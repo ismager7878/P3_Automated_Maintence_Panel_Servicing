34820e9c244c96b687969b003a4f42e3e2a18321
Benjamin Hermansen <bshermansen@gmail.com>
2025-11-05 10:06:28 +0100

    updated test script

--- src/amps_python/amps_python/resources/test.py
+++ src/amps_python/amps_python/resources/test.py
@@ -1,5 +1,5 @@
 from border_detector_color import detect_border
-import csv, json, os
+import csv, os, json
 import numpy as np
 import cv2 as cv
 
@@ -15,6 +15,7 @@ with open("datasets/test_images_dataset/training_paths.csv", "r") as file:
         print(f"Reading images in: {line[0]}", end="\r", flush=True)
         color_paths.append(os.path.join(line[0], "color.png"))
         depth_paths.append(os.path.join(line[0], "depth.png"))
+print("\nDone reading CSV file")
 
 # Load images from paths
 color_images = np.array([])
@@ -35,11 +36,18 @@ for i, path in enumerate(depth_paths):
 print()
 
 #Load ground truths
-ground_truth_btn_config_1 = json.load("datasets/test_images_dataset/btn_config_1/ground_truth.json")
-ground_truth_btn_config_2 = json.load("datasets/test_images_dataset/btn_config_2/ground_truth.json")
-ground_truth_btn_config_3 = json.load("datasets/test_images_dataset/btn_config_3/ground_truth.json")
-ground_truth_btn_config_4 = json.load("datasets/test_images_dataset/btn_config_4/ground_truth.json")
-ground_truth_btn_config_5 = json.load("datasets/test_images_dataset/btn_config_5/ground_truth.json")
+print("Loading ground truth data")
+with open("datasets/test_images_dataset/btn_config_1/ground_truth.json", "r") as json_file:
+    ground_truth_btn_config_1 = json.load(json_file)
+with open("datasets/test_images_dataset/btn_config_2/ground_truth.json", "r") as json_file:
+    ground_truth_btn_config_2 = json.load(json_file)
+with open("datasets/test_images_dataset/btn_config_3/ground_truth.json", "r") as json_file:
+    ground_truth_btn_config_3 = json.load(json_file)
+with open("datasets/test_images_dataset/btn_config_4/ground_truth.json", "r") as json_file:
+    ground_truth_btn_config_4 = json.load(json_file)
+with open("datasets/test_images_dataset/btn_config_5/ground_truth.json", "r") as json_file:
+    ground_truth_btn_config_5 = json.load(json_file)
+print("Done loading ground truth data")
 
 if __name__ == "__main__":
     for image in color_images:
