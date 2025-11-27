d95e260724042910c82220b7c0769b3c4f67bf2c
Benjamin Hermansen <bshermansen@gmail.com>
2025-11-27 09:33:07 +0100

    Update test and training paths with new dataset entries and adjust CSV writing logic

--- src/amps_python/amps_python/resources/generatePaths.py
+++ src/amps_python/amps_python/resources/generatePaths.py
@@ -5,10 +5,10 @@ import natsort
 paths = []
 
 dataset_dir = "datasets/auto_aligned_dataset/"
-paths = glob.glob(dataset_dir + "*/*/")
-
+paths = glob.glob(dataset_dir + "**/color.png", recursive=True)
 
 paths = natsort.natsorted(paths)
+paths = [path.replace("/color.png", "") for path in paths]
 
 paths_test = [path for i, path in enumerate(paths) if i % 3 == 0]
 
@@ -29,11 +29,9 @@ for path in paths_training:
 with open("datasets/auto_aligned_dataset/test_paths.csv", "w", newline='') as test_file:
     csv_writer = csv.writer(test_file)
     for path in paths_test:
-        dir_path = os.path.dirname(path)
-        csv_writer.writerow([dir_path])
+        csv_writer.writerow([path])
 
 with open("datasets/auto_aligned_dataset/training_paths.csv", "w", newline='') as training_file:
     csv_writer = csv.writer(training_file)
     for path in paths_training:
-        dir_path = os.path.dirname(path)
-        csv_writer.writerow([dir_path])
+        csv_writer.writerow([path])
