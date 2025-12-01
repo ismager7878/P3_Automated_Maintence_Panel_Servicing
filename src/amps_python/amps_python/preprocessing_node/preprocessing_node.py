6c6f196b2ed5fc7f6880450d9d77dc04f5790183
Benjamin Hermansen <bshermansen@gmail.com>
2025-12-01 15:08:53 +0100

    Refactor depth transformation logic and improve error handling in PreprocessingNode

--- src/amps_python/amps_python/preprocessing_node/preprocessing_node.py
+++ src/amps_python/amps_python/preprocessing_node/preprocessing_node.py
@@ -29,7 +29,10 @@ class PreprocessingNode(Node):
         color_image = self.bridge.imgmsg_to_cv2(msg.rgb_frame, desired_encoding='passthrough')
         
         # Process the depth image
-        transformed_depth_image, transform_matrix = self.transform_depth(depth_image)
+        transformed_depth_image, transform_matrix = transform_depth(depth_image)
+        if transform_matrix is None:
+            self.get_logger().warning('Transformation matrix could not be computed. Skipping this frame.')
+            return
         transformed_color_image = cv.warpPerspective(color_image, transform_matrix, (transformed_depth_image.shape[1], transformed_depth_image.shape[0]))
         
         # Convert transformed depth to ROS Image message
@@ -42,82 +45,6 @@ class PreprocessingNode(Node):
         self.publisher_depth.publish(transformed_depth_msg)
         self.publisher_color.publish(transformed_color_msg)
         self.get_logger().info('Published transformed depth and color image')
-
-    def transform_depth(self, img, show=False):
-        # Calculate median of ROI
-        adjustedImg = np.array(img, dtype=np.uint16)
-        vals = adjustedImg[280:780, 70:570]
-        median_of_roi = np.median(vals)
-        
-        # Scaling
-        min_val, max_val = median_of_roi-26, median_of_roi+7
-        adjustedImg = ((img.astype(np.float32) - min_val) / (max_val - min_val)) * 255
-        
-        # Clipping
-        adjustedImg[adjustedImg < 0] = 0
-        adjustedImg[adjustedImg > 255] = 0
-        adjustedImg = adjustedImg.astype(np.uint8)
-        if show:
-            cv.imshow("Adjusted Depth Image", adjustedImg)
-            cv.waitKey(0)
-            cv.destroyAllWindows()
-            
-        # Segmentation
-        _, binaryImg = cv.threshold(adjustedImg, 1, 255, cv.THRESH_BINARY)
-        binaryImg = cv.medianBlur(binaryImg, 5)
-        #binaryImg = cv.morphologyEx(binaryImg, cv.MORPH_CLOSE, kernel)
-        #binaryImg = cv.morphologyEx(binaryImg, cv.MORPH_OPEN, kernel)
-        if show:
-            cv.imshow("Binary Depth Image", binaryImg)
-            cv.waitKey(0)
-            cv.destroyAllWindows()
-
-        try:
-            # Find contours and keep the largest
-            contours, _ = cv.findContours(binaryImg, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-            largest = max(contours, key=cv.contourArea)
-            #largest = sorted(contours, key=cv.contourArea)
-
-            # Bounding box
-            rect = cv.minAreaRect(largest)     # rotated rectangle
-            box = cv.boxPoints(rect)           # 4 corner points
-            box = np.int32(box)
-            box = self.order_points(box)  # order points: top-left, top-right, bottom-right, bottom-left
-        except Exception as ex:
-            self.get_logger().error(f"Error in preprocessing node, contour detection: {ex}")
-            return adjustedImg, np.eye(3)
-
-        # Draw corner points and lines
-        if show:
-            print("Corner Points:\n", box)
-            print(box[0])
-            adjustedImgBGR = cv.cvtColor(adjustedImg, cv.COLOR_GRAY2BGR)
-            cv.polylines(adjustedImgBGR, [box.astype(np.int32)], True, (0,255,0), 3)
-            cv.imshow("Corner Points and lines", adjustedImgBGR)
-            cv.waitKey(0)
-            cv.destroyAllWindows()
-
-        
-        # Transformation
-        corner_points = np.array(box, dtype="float32")
-        dst_points = np.array([[0,0],[684,0],[684,861],[0,861]],dtype="float32")
-        transform_matrix = cv.getPerspectiveTransform(corner_points, dst_points)
-        warped_img = cv.warpPerspective(adjustedImg, transform_matrix, (861, 684))
-
-        return warped_img, transform_matrix
-
-    def order_points(self, points):
-        box = np.zeros((4,2), dtype="float32")
-
-        sum = points.sum(axis=1)
-        box[0] = points[np.argmin(sum)]     # top-left
-        box[2] = points[np.argmax(sum)]     # bottom-right
-
-        diff = np.diff(points, axis=1)
-        box[1] = points[np.argmin(diff)]  # top-right
-        box[3] = points[np.argmax(diff)]  # bottom-left
-
-        return box # return the ordered coordinates like: top-left, top-right, bottom-right, bottom-left
         
 def main():
     rclpy.init()
@@ -125,3 +52,78 @@ def main():
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
+def transform_depth(img, show=False):
+    # Calculate median of ROI
+    adjustedImg = np.array(img, dtype=np.uint16)
+    vals = adjustedImg[280:780, 70:570]
+    median_of_roi = np.median(vals)
+    
+    # Scaling
+    min_val, max_val = median_of_roi-26, median_of_roi+7
+    adjustedImg = ((img.astype(np.float32) - min_val) / (max_val - min_val)) * 255
+    
+    # Clipping
+    adjustedImg[adjustedImg < 0] = 0
+    adjustedImg[adjustedImg > 255] = 0
+    adjustedImg = adjustedImg.astype(np.uint8)
+    if show:
+        cv.imshow("Adjusted Depth Image", adjustedImg)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+        
+    # Segmentation
+    _, binaryImg = cv.threshold(adjustedImg, 1, 255, cv.THRESH_BINARY)
+    binaryImg = cv.medianBlur(binaryImg, 5)
+    #binaryImg = cv.morphologyEx(binaryImg, cv.MORPH_CLOSE, kernel)
+    #binaryImg = cv.morphologyEx(binaryImg, cv.MORPH_OPEN, kernel)
+    if show:
+        cv.imshow("Binary Depth Image", binaryImg)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
+    try:
+        # Find contours and keep the largest
+        contours, _ = cv.findContours(binaryImg, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+        largest = max(contours, key=cv.contourArea)
+        #largest = sorted(contours, key=cv.contourArea)
+
+        # Bounding box
+        rect = cv.minAreaRect(largest)     # rotated rectangle
+        box = cv.boxPoints(rect)           # 4 corner points
+        box = np.int32(box)
+        box = order_points(box)  # order points: top-left, top-right, bottom-right, bottom-left
+    except Exception as ex:
+        return None
+
+    # Draw corner points and lines
+    if show:
+        print("Corner Points:\n", box)
+        print(box[0])
+        adjustedImgBGR = cv.cvtColor(adjustedImg, cv.COLOR_GRAY2BGR)
+        cv.polylines(adjustedImgBGR, [box.astype(np.int32)], True, (0,255,0), 3)
+        cv.imshow("Corner Points and lines", adjustedImgBGR)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
+    
+    # Transformation
+    corner_points = np.array(box, dtype="float32")
+    dst_points = np.array([[0,0],[883,0],[883,681],[0,681]],dtype="float32")
+    transform_matrix = cv.getPerspectiveTransform(corner_points, dst_points)
+    warped_img = cv.warpPerspective(adjustedImg, transform_matrix, (883, 681))
+
+    return warped_img, transform_matrix
+
+def order_points(points):
+    box = np.zeros((4,2), dtype="float32")
+
+    sum = points.sum(axis=1)
+    box[0] = points[np.argmin(sum)]     # top-left
+    box[2] = points[np.argmax(sum)]     # bottom-right
+
+    diff = np.diff(points, axis=1)
+    box[1] = points[np.argmin(diff)]  # top-right
+    box[3] = points[np.argmax(diff)]  # bottom-left
+
+    return box # return the ordered coordinates like: top-left, top-right, bottom-right, bottom-left
--- src/amps_python/amps_python/resources/test.py
+++ src/amps_python/amps_python/resources/test.py
@@ -1,7 +1,89 @@
-from preprocessing_node.preprocessing_node import PreprocessingNode
 import csv, os, json
 import numpy as np
 import cv2 as cv
+
+# ---------------------- Insert code to be tested here ----------------------
+
+
+def transform_depth(img, show=False):
+    # Calculate median of ROI
+    adjustedImg = np.array(img, dtype=np.uint16)
+    vals = adjustedImg[280:780, 70:570]
+    median_of_roi = np.median(vals)
+    
+    # Scaling
+    min_val, max_val = median_of_roi-26, median_of_roi+7
+    adjustedImg = ((img.astype(np.float32) - min_val) / (max_val - min_val)) * 255
+    
+    # Clipping
+    adjustedImg[adjustedImg < 0] = 0
+    adjustedImg[adjustedImg > 255] = 0
+    adjustedImg = adjustedImg.astype(np.uint8)
+    if show:
+        cv.imshow("Adjusted Depth Image", adjustedImg)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+        
+    # Segmentation
+    _, binaryImg = cv.threshold(adjustedImg, 1, 255, cv.THRESH_BINARY)
+    binaryImg = cv.medianBlur(binaryImg, 5)
+    #binaryImg = cv.morphologyEx(binaryImg, cv.MORPH_CLOSE, kernel)
+    #binaryImg = cv.morphologyEx(binaryImg, cv.MORPH_OPEN, kernel)
+    if show:
+        cv.imshow("Binary Depth Image", binaryImg)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
+    try:
+        # Find contours and keep the largest
+        contours, _ = cv.findContours(binaryImg, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+        largest = max(contours, key=cv.contourArea)
+        #largest = sorted(contours, key=cv.contourArea)
+
+        # Bounding box
+        rect = cv.minAreaRect(largest)     # rotated rectangle
+        box = cv.boxPoints(rect)           # 4 corner points
+        box = np.int32(box)
+        box = order_points(box)  # order points: top-left, top-right, bottom-right, bottom-left
+    except Exception as ex:
+        return None
+
+    # Draw corner points and lines
+    if show:
+        print("Corner Points:\n", box)
+        print(box[0])
+        adjustedImgBGR = cv.cvtColor(adjustedImg, cv.COLOR_GRAY2BGR)
+        cv.polylines(adjustedImgBGR, [box.astype(np.int32)], True, (0,255,0), 3)
+        cv.imshow("Corner Points and lines", adjustedImgBGR)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
+    
+    # Transformation
+    corner_points = np.array(box, dtype="float32")
+    dst_points = np.array([[0,0],[883,0],[883,681],[0,681]],dtype="float32")
+    transform_matrix = cv.getPerspectiveTransform(corner_points, dst_points)
+    warped_img = cv.warpPerspective(adjustedImg, transform_matrix, (883, 681))
+
+    return warped_img, transform_matrix
+
+def order_points(points):
+    box = np.zeros((4,2), dtype="float32")
+
+    sum = points.sum(axis=1)
+    box[0] = points[np.argmin(sum)]     # top-left
+    box[2] = points[np.argmax(sum)]     # bottom-right
+
+    diff = np.diff(points, axis=1)
+    box[1] = points[np.argmin(diff)]  # top-right
+    box[3] = points[np.argmax(diff)]  # bottom-left
+
+    return box # return the ordered coordinates like: top-left, top-right, bottom-right, bottom-left
+
+
+
+# ---------------------- End of test code ----------------------
+
 
 # Load image paths from file
 #! Is currently set to load training data
@@ -50,9 +132,8 @@ with open("datasets/test_images_dataset/btn_config_5/ground_truth.json", "r") as
 print("Done loading ground truth data")
 
 if __name__ == "__main__":
-    preprocessing_node = PreprocessingNode()
     for depthImg, colorImg in zip(depth_images, color_images):
-        croppedDepth, M = preprocessing_node.transform_depth(depthImg, False)
+        croppedDepth, M = transform_depth(depthImg, False)
         croppedColor = cv.warpPerspective(colorImg, M, (croppedDepth.shape[1], croppedDepth.shape[0]))
         depthColored = cv.applyColorMap(croppedDepth, cv.COLORMAP_JET)
         cv.imshow("Transformed Color Image", croppedColor)
