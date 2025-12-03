df19092028d2a8e67d105f041271efb13f709774
Petur <phamme24@student.aau.dk>
2025-12-03 15:37:20 +0100

    launch fix

--- src/amps_python/launch/preprocessing.launch.py
+++ src/amps_python/launch/preprocessing.launch.py
@@ -3,7 +3,7 @@ from launch_ros.actions import Node
 import os
     
 def generate_launch_description():
-    ws_path = os.path.expanduser("~/Github/P3_Automated_Maintence_Panel_Servicing/.venv")
+    ws_path = os.path.expanduser("~/Documents/Github/P3_Automated_Maintence_Panel_Servicing/.venv")
     venv_python = os.path.join(ws_path, "bin", "python3")
     
     # # Automatically find the workspace root relative to this launch file to locate the .venv
